761746e4c0a3024ea8f92bb33e6f5bd67d30c148
Robert T <RobertJTilton@gmail.com>
2023-10-30 15:21:36 -0400

    Initialization

---
+++ auth_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+   path('', views.Home, name='home'),
+   path('home/', views.Home, name='home'),
+   path('login/', views.Login, name='login' ),
+   path('logout/', views.Logout, name='logout'),
+   path('registration/', views.Registration, name='registration'),
+
+]
---
+++ auth_app/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from . import models
+
+def Home(request):
+    return render(request, 'home.html')
+
+
+def Registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    
+    else:
+        form = UserCreationForm()
+
+    return render(request , 'registration.html', {'form': form})
+
+
+def Login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user != None:
+                login(request, user)
+                return redirect('home')
+    
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+    
+
+def Logout(request):
+    logout(request)
+    return redirect('login')
