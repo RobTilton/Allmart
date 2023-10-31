5d183d38b7c92b46ee16ecc191d1498c03d9002e
Robert T <RobertJTilton@gmail.com>
2023-10-31 14:21:18 -0400

    revert

--- Allmart/settings.py
+++ Allmart/settings.py
@@ -38,18 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    
 
     'auth_app'
 ]
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
