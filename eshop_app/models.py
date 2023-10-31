52cd165a2cec84a331a7e3788829c7723b91407c
Robert T <RobertJTilton@gmail.com>
2023-10-31 15:11:52 -0400

    modification to database, SsoT

--- eshop_app/models.py
+++ eshop_app/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cents = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
@@ -22,4 +22,4 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
